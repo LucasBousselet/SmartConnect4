81e826e6e63f6981a9f98a0c93ec6146ceba9934
Lucas <BousseletLucas@gmail.com>
2017-04-13 16:07:54 -0400

    Creation of Cell class, and import of cell sprites for red/yellow/empty colors

---
+++ Connect4/Connect4/Cell.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Connect4
+{
+    /// <summary>
+    /// Logique d'interaction pour Cell.xaml
+    /// </summary>
+    public partial class Cell : UserControl
+    {
+        private bool isRed = new bool();
+
+        private bool isYellow = new bool();
+
+        private bool isEmpty = new bool();
+
+        public Cell()
+        {
+            InitializeComponent();
+            isEmpty = true;
+            isRed = false;
+            isYellow = false;
+        }
+
+        public bool IsRed
+        {
+            get
+            {
+                return isRed;
+            }
+            set
+            {
+                isRed = value;
+            }
+        }
+
+        public bool IsYellow
+        {
+            get
+            {
+                return isYellow;
+            }
+            set
+            {
+                isYellow = value;
+            }
+        }
+
+        public bool IsEmpty
+        {
+            get
+            {
+                return isEmpty;
+            }
+            set
+            {
+                isEmpty = value;
+                isRed = false;
+                isYellow = false;
+            }
+        }
+    }
+}
