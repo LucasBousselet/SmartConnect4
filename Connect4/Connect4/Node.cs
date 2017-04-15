011ccbff2eac86fd38d019450c35cb962f248dfc
Lucas <BousseletLucas@gmail.com>
2017-04-15 18:19:21 -0400

    Add Node class

---
+++ Connect4/Connect4/Node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect4
+{
+    class Node
+    {
+        String m_WhoseTurnItIs = String.Empty;
+
+        GameGrid m_Grid;
+
+        int m_Depth = -1;
+
+        public Node(String p_PlayerColor, GameGrid p_Grid, int m_depth)
+        {
+            m_WhoseTurnItIs = p_PlayerColor;
+            m_Grid = p_Grid;
+            m_Depth = m_depth;
+        }
+    }
+}
